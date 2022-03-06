16ecf419b98aa828890b84e292714dd77dd743b9
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-03-06 16:51:13 +0530

    [Feat]: Created user model
    - hashing the password
    - generating jwt token
    - authorizing user
    - checking password

---
+++ server/src/models/user.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+// Library
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A user must have a name"],
+      trim: true,
+      maxlength: [50, "Name must be less than 50 characters"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: [true, "A user must have a unique email"],
+      lowercase: true,
+      maxlength: [50, "Email must be less than 50 characters"],
+    },
+    password: {
+      type: String,
+      trim: true,
+      minlength: [7, "A password must contain at least 7 characters"],
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: {
+        values: ["user", "admin"],
+        message: "Please select from this category",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hashing Password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Getting jwt login token
+userSchema.methods.getJwtLoginToken = async function () {
+  const token = jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: process.env.JWT_EXPIRY,
+    }
+  );
+
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+  return token;
+};
+
+// Checking is valid password
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Exporting Model
+module.exports = mongoose.model("User", userSchema);
