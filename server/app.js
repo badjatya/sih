1bae38fa95ac6d6ea274b8cd0ee031858eb72000
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-03-10 21:11:49 +0530

    [Feat]: Configured cookieparser and fileupload

--- server/app.js
+++ server/app.js
@@ -2,9 +2,20 @@ require("dotenv").config({ path: "./src/config/.env" });
 const express = require("express");
 const app = express();
 
+// Library
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ name: "Server", message: "Hello from server!" });
