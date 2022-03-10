688268207730e54543e350be31c8335dd77a638c
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-03-10 21:14:30 +0530

    [Chore]: Configured cookie

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -63,6 +63,14 @@ exports.login = async (req, res) => {
     // Valid user, creating jwt token valid for 2days
     const token = await user.getJwtLoginToken();
 
+    // Sending a cookie valid for 2days
+    res.cookie("token", token, {
+      expires: new Date(
+        Date.now() * process.env.COOKIE_TIME * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    });
+
     // Sending response
     res.json({
       status: "success",
