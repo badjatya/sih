beac64cee9bea1b5014d595ad27d36403657fad3
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-03-13 22:54:53 +0530

    [Feat]: Getting all list of institutes with filters and sorting

--- server/src/controllers/institute.js
+++ server/src/controllers/institute.js
@@ -3,6 +3,7 @@ const Institute = require("../models/institute");
 
 // Utils
 const customError = require("../utils/customError");
+const instituteQueryHandler = require("../utils/instituteQueryHandler");
 
 // Creating a new Institute
 exports.createInstitute = async (req, res) => {
@@ -62,6 +63,33 @@ exports.createInstitute = async (req, res) => {
       status: "success",
       message: "Institute created successfully",
       institute,
+    });
+  } catch (error) {
+    customError(res, 500, error.message, "error");
+  }
+};
+
+// Get all institutes
+exports.getAllInstitutes = async (req, res) => {
+  try {
+    let institutes = new instituteQueryHandler(
+      Institute.find({}).select(["_id", "name", "location", "type", "images"]),
+      req.query
+    )
+      .search()
+      .filter();
+    const totalInstitutes = await Institute.countDocuments();
+    const totalProductPerPage = 20;
+
+    institutes.pager(totalProductPerPage);
+    institutes = await institutes.base;
+
+    // response
+    res.status(200).json({
+      status: "success",
+      total: totalInstitutes,
+      result: institutes.length,
+      institutes,
     });
   } catch (error) {
     customError(res, 500, error.message, "error");
--- server/src/routes/institute.js
+++ server/src/routes/institute.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 
 // User Controller
-const { createInstitute } = require("../controllers/institute");
+const {
+  createInstitute,
+  getAllInstitutes,
+} = require("../controllers/institute");
 
 // User middleware
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 // Institute
 router.route("/create").post(isLoggedIn, customRole("admin"), createInstitute);
+router.route("/").get(isLoggedIn, getAllInstitutes);
 
 module.exports = router;
