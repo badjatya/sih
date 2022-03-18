bc44e775a7594a72753adfdae51e7879c1b41f3f
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-03-18 12:21:06 +0530

    [Feat]: Connecting cloudinary

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const app = require("./app");
 // Connecting to DB
 require("./src/database/db").dbConnect();
 
+// Lib
+const cloudinary = require("cloudinary");
+
+// Cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+  secure: true,
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`Listening at http://localhost:${process.env.PORT}`)
 );
